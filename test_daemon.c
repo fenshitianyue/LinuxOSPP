c399b1baaf47838767ce226a68e5df913af0c05e
fenshitianyue <abzanda@163.com>
2019-03-16 14:21:27 +0800

    update code

--- test_daemon.c
+++ test_daemon.c
@@ -1,8 +1,34 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+typedef enum{TRUE, FALSE} _bool;
 
+_bool daemonize(){
+  pid_t pid = fork();
+  if(pid < 0){
+    return FALSE;
+  }else if(pid < 0){
+    exit(EXIT_SUCCESS);
+  }
+  umask(0);
+  if(setsid() < 0){
+    return FALSE;
+  }
+  if(chdir("/") < 0){
+    return FALSE;
+  }
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
 
+  //不知道为什么这个会报错？
+  open("/dev/null", O_RDONLY);
+  open("/dev/null", O_RDWR);
+  open("/dev/null", O_RDWR);
+  return TRUE;
+}
 
 int main() {
   

f42fcf559d995a76f6daf40d784b623d8e33116c
fenshitianyue <abzanda@163.com>
2019-06-28 15:24:35 +0800

    producter and consumer module

---
+++ test_thread/consumer_producter_module.cc
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <queue>
+#include <cstdlib>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NUM 8
+
+class BlockQueue{
+private:
+  std::queue<int> q;
+  size_t cap;
+  pthread_mutex_t lock;
+  pthread_cond_t full;
+  pthread_cond_t empty;
+private:
+  void LockQueue(){
+    pthread_mutex_lock(&lock);
+  }
+  void UnLockQueue(){
+    pthread_mutex_unlock(&lock);
+  }
+  void ProductWait(){
+    pthread_cond_wait(&full, &lock);
+  }
+  void ConsumerWait(){
+    pthread_cond_wait(&empty, &lock);
+  }
+  void NotifyProduct(){
+    pthread_cond_signal(&full);
+  }
+  void NotifyConsummer(){
+    pthread_cond_signal(&empty);
+  }
+  bool IsEmpty(){
+    return q.size() == 0;
+  }
+  bool IsFull(){
+    return q.size() == cap;
+  }
+public:
+  BlockQueue(int _cap = NUM) : cap(_cap){
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&full, NULL);
+    pthread_cond_init(&empty, NULL);
+  }
+  void PushData(const int& data){
+    LockQueue();
+    while(IsFull()){
+      NotifyConsummer();
+      std::cout << "queue full, notify consumer. product stop!" << std::endl;
+      ProductWait();
+    }
+    q.push(data);
+    NotifyConsummer();
+    UnLockQueue();
+  }
+  void PopData(int& data){
+    LockQueue();
+    while(IsEmpty()){
+      NotifyProduct();
+      std::cout << "queue empty, notify producter. consummer stop!" << std::endl;
+      ConsumerWait();
+    }
+    data = q.front();
+    q.pop();
+    NotifyProduct();
+    UnLockQueue();
+  }
+  ~BlockQueue(){
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&full);
+    pthread_cond_destroy(&empty);
+  }
+};
+
+void* consumer(void* arg){
+  BlockQueue* b = (BlockQueue*)arg;
+  int data;
+  while(true){
+    b->PopData(data);
+    std::cout << "Consum data done: " << data << std::endl;
+  }
+}
+
+void* producter(void* arg){
+  BlockQueue* b = (BlockQueue*)arg;
+  srand((unsigned long)time(NULL));
+  while(true){
+    int data = rand() % 1024;
+    b->PushData(data);
+    std::cout << "Product data done: " << data << std::endl;
+    sleep(1);
+  }
+}
+
+int main(){
+  BlockQueue b;
+  pthread_t c, p;
+  pthread_create(&c, NULL, consumer, (void*)&b);
+  pthread_create(&p, NULL, producter, (void*)&b);
+
+  pthread_join(c, NULL);
+  pthread_join(p, NULL);
+  return 0;
+}
